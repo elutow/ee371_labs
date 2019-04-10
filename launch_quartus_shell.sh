b87d6a80dbcdb50c6447402cec391ea5a646d615
Elton <eloston@uw.edu>
2019-04-10 11:44:27 -0700

    Update launch_* scripts to work anywhere inside ~/VMTMP

--- launch_quartus_shell.sh
+++ launch_quartus_shell.sh
@@ -2,4 +2,12 @@
 
 # To be executed on host, using container
 
-QUARTUSCMD_START_DIR=/vmtmp/ee371_labs ~/core/scripts/containers/debianquartus64_quartuscmd.sh bash
+_current_dir=$(dirname $(readlink -f $0))
+_relative_path=$(realpath -e --relative-to=$(readlink -f ~/VMTMP) $_current_dir)
+
+if [[ $_relative_path == ..* ]]; then
+	printf 'ERROR: Path is not inside %s: %s\n' "~/VMTMP" "$_relative_path"
+	exit 1
+fi
+
+QUARTUSCMD_START_DIR="/vmtmp/$_relative_path" ~/core/scripts/containers/debianquartus64_quartuscmd.sh bash
