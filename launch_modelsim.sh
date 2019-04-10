f65552841681335677970e1c766bd9a84b5c8a19
Elton <eloston@uw.edu>
2019-04-10 11:44:27 -0700

    Update launch_* scripts to work anywhere inside ~/VMTMP

--- launch_modelsim.sh
+++ launch_modelsim.sh
@@ -2,4 +2,12 @@
 
 # To be executed on host, using container
 
-~/core/scripts/containers/debianquartus64_cmd.sh '/vmtmp/ee371_labs' 'LD_LIBRARY_PATH=/home/wineuser/intelFPGA_lite/lib32' '/home/wineuser/intelFPGA_lite/18.1/modelsim_ase/linuxaloem/vsim'
+_current_dir=$(dirname $(readlink -f $0))
+_relative_path=$(realpath -e --relative-to=$(readlink -f ~/VMTMP) $_current_dir)
+
+if [[ $_relative_path == ..* ]]; then
+	printf 'ERROR: Path is not inside %s: %s\n' "~/VMTMP" "$_relative_path"
+	exit 1
+fi
+
+~/core/scripts/containers/debianquartus64_cmd.sh "/vmtmp/$_relative_path" 'LD_LIBRARY_PATH=/home/wineuser/intelFPGA_lite/lib32' '/home/wineuser/intelFPGA_lite/18.1/modelsim_ase/linuxaloem/vsim'
